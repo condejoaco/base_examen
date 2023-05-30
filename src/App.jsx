6d87e479edd4746063d5b346bea07dbcf7bc6279
condejoaco <condejoaco@gmail.com>
2023-05-30 19:20:14 -0300

    Modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
 import Card from './components/Card'
+import { useState } from 'react'
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [formulario, setFormulario] = useState(false);
+
+  const pepito = e=>{setNombre(e.target.value)}
+
+  const handleSubmit= event => {
+    event.preventDefault()
+    if(nombre === ""){
+      alert("esto esta mal")
+    } else{
+      setFormulario(true)
+    }
+  }
 
   return (
     <>
       <div className='App'>
         <h1>Carga de Estudiantes</h1>
-        <form></form>
+        <form onSubmit={handleSubmit}>
+          <label> Nombre </label>
+            <input
+              value={nombre} //Inicial
+              onChange={pepito}
+            />
+            <input type="submit"/>
+          
+        </form>
         <Card/>
       </div>
     </>
